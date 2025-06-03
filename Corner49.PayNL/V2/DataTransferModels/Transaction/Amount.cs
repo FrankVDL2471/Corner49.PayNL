6e40b8d84d1cfab699eccbf270ce38a504ad80ec
Frank Vanderlinden <frankvdl@gmail.com>
2025-06-03 11:58:46 +0200

    Amount Constructor

--- Corner49.PayNL/V2/DataTransferModels/Transaction/Amount.cs
+++ Corner49.PayNL/V2/DataTransferModels/Transaction/Amount.cs
@@ -3,10 +3,20 @@ using System.Text.Json.Serialization;
 namespace Corner49.PayNL.V2.DataTransferModels.Transaction;
 
 public class Amount {
+
+	public Amount()
+	{
+		this.Currency = "EUR";
+	}
+	public Amount(decimal value) : this()
+	{
+		this.Value = (int)Math.Floor(value * 100);
+	}
+
 	// Amount in cents
 	[JsonPropertyName("value")]
 	public int? Value { get; set; }
 
 	[JsonPropertyName("currency")]
-	public string? Currency { get; set; }
+	public string Currency { get; set; }
 }
